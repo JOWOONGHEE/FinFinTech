6db96d97b130b576b549df56588836a1c15d9943
WoongHee Jo <xcv415@naver.com>
2024-06-23 23:49:40 +0900

    Initial Commit

---
+++ src/main/java/com/fin/finfintech/FinfintechApplication.java
@@ -0,0 +1,13 @@
+package com.fin.finfintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinfintechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinfintechApplication.class, args);
+	}
+
+}
